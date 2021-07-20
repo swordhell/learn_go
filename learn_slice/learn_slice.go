2ccb399895e93957db77a0e7f4bb01ea0260e13a
abel <xiaozanbiao@xiaoniuhy.com>
2021-07-20 20:31:36 +0800

    测试slice;

--- learn_slice/learn_slice.go
+++ learn_slice/learn_slice.go
@@ -9,13 +9,24 @@ func main() {
 		s = append(s, i)
 	}
 	fmt.Printf("1 %#v\n", s)
+
+	// 去掉头
+	s = s[1:]
+	fmt.Println(s)
+
+	// 取出第一个
 	s = s[0:1]
 	fmt.Printf("2 %#v\n", s)
+
+	// 清空
 	s = s[:0]
 	fmt.Printf("3 %#v\n", s)
+
 	for i := 0; i < 10; i++ {
 		s = append(s, i)
 	}
+
+	// 最后一个元素
 	s = s[len(s)-1:]
 	fmt.Printf("4 %#v\n", s)
 }
